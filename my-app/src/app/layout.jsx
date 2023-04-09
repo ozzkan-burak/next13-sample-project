55997a9f35260795dcd2e2267abdebca7422c845
ozzkan-burak <brk.ozzkan@gmail.com>
2023-04-09 11:04:19 +0300

    episode3

--- my-app/src/app/layout.jsx
+++ my-app/src/app/layout.jsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import './styles/reset.css';
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -7,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="tr">
       <body>{children}</body>
     </html>
   )
