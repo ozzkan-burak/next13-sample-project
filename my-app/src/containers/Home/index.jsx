9b7d2096b2461341748e690ca6f712f41ac034f5
ozzkan-burak <brk.ozzkan@gmail.com>
2023-04-30 00:28:22 +0300

    episode15

--- my-app/src/containers/Home/index.jsx
+++ my-app/src/containers/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Movies from "@/mocks/movies.json";
+import Genres from "@/mocks/genres.json";
 import FeaturedMovie from '@/components/FeaturedMovie';
+import Categories from '@/components/Categories';
 
 const HomeContainer = () => {
-  console.log(Movies.results[0])
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]} />
+      <Categories categories={Genres.genres.slice(0,5)}/>
     </div>
   )
 }
