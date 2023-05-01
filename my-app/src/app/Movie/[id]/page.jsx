8af6ebfb762d5dd8a7704d65a930b5dbadbeedda
ozzkan-burak <brk.ozzkan@gmail.com>
2023-05-01 19:01:33 +0300

    episode19

--- my-app/src/app/Movie/[id]/page.jsx
+++ my-app/src/app/Movie/[id]/page.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { notFound } from "next/navigation";
 import MovieContainer from "@/containers/Movie";
 import Movies from "@/mocks/movies.json";
 
 const MoviePage = ({params}) => {
   
   const movieDetail = Movies.results.find((movie)=>movie.id.toString() === params.id);
+
+  if(!movieDetail) {
+    notFound()
+  }
 
   return (
     <MovieContainer movie={movieDetail} />
